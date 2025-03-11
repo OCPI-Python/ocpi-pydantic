139869a8422d9eeca06d0476735600ffc7d8cf5c
Leon <leon0824@gmail.com>
2025-03-11 17:48:07 +0800

    Updeate models definition
    Updeate models definition

--- src/ocpi_pydantic/v221/locations/connector.py
+++ src/ocpi_pydantic/v221/locations/connector.py
@@ -12,8 +12,8 @@ class OcpiConnector(BaseModel):
     standard: OcpiConnectorTypeEnum = Field(description='The standard of the installed connector.')
     format: OcpiConnectorFormatEnum = Field(description='The format (socket/cable) of the installed connector.')
     power_type: OcpiPowerTypeEnum
-    max_voltage: int = Field(description='Maximum voltage of the connector (line to neutral for AC_3_PHASE), in volt [V].')
-    max_amperage: int = Field(description='Maximum amperage of the connector, in ampere [A].')
+    max_voltage: int = Field(description='Maximum voltage of the connector (line to neutral for AC_3_PHASE), in volt [V].', gt=0)
+    max_amperage: int = Field(description='Maximum amperage of the connector, in ampere [A].', gt=0)
     max_electric_power: int | None = Field(None, description='Maximum electric power that can be delivered by this connector, in Watts (W).')
     tariff_ids: list[str] = Field([], description='Identifiers of the currently valid charging tariffs.')
     terms_and_conditions: HttpUrl | None = Field(None, description='URL to the operator’s terms and conditions.')
