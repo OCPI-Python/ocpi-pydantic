5ea0228e8ae95475c9e4147eeac30bddca429331
Leon <leon0824@gmail.com>
2025-06-10 15:56:35 +0800

    Updeate models definition

--- src/ocpi_pydantic/v221/base.py
+++ src/ocpi_pydantic/v221/base.py
@@ -30,13 +30,15 @@ class OcpiPrice(BaseModel):
     @field_validator('excl_vat', mode='after')
     @classmethod
     def validate_excl_vat(cls, value: Decimal, info: ValidationInfo):
+        if value == Decimal('Infinity'): return value
         return value.quantize(Decimal('0.001'), ROUND_HALF_UP)
     
     @field_validator('incl_vat', mode='after')
     @classmethod
     def validate_incl_vat(cls, value: Decimal | None, info: ValidationInfo):
         if value == None: return None
-        else: return value.quantize(Decimal('0.001'), ROUND_HALF_UP)
+        if value == Decimal('Infinity'): return value
+        return value.quantize(Decimal('0.001'), ROUND_HALF_UP)
 
 
 
